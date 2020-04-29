1f0ba16b9b41c22752e193280bb14f7bcca12775
arun <503100764@logon.ds.ge.com>
2020-04-29 20:26:37 +0530

    product styling

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -3,41 +3,59 @@ import { Grid, Button } from "@material-ui/core"
 import * as cartTypes from "../../redux/reducers/cart/types"
 import "./product.module.scss"
 import PropTypes from "prop-types"
-const Product = (props) => (
-  <Grid
-    container
-    direction="column"
-    justify="center"
-    alignItems="center"
-    className={"productContainer"}
-  >
-    <Grid item>
-      <img alt={props.name} src={props.image} height="50%" width="50%" />
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+  productButton: {
+    backgroundColor: "orange",
+    color: "black",
+    padding: "5px 10px",
+    margin: "10px",
+    borderRadius: "20px",
+  },
+})
+const Product = (props) => {
+  const classes = useStyles()
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="stretch"
+      className={"productContainer"}
+    >
+      <Grid item className={"productImage"}>
+        <img alt={props.name} src={props.image} height="50%" width="50%" />
+      </Grid>
+      <Grid item xs={12} className={"name"}>
+        <span>{props.name}</span>
+      </Grid>
+      <Grid item>
+        <span className={"actual"}>&#8377;{props.actual}</span>
+        <span className={"display"}>{props.display}</span>
+        <span className={"discount"}>{props.discount}%off</span>
+      </Grid>
+      <Grid item className={'button'}>
+        <Button
+          className={classes.productButton}
+          onClick={() => {
+            props.cartItemList.indexOf(props.productNo) == -1
+              ? props.add({
+                  productNo: props.productNo,
+                  count: 1,
+                  name: props.name,
+                  image: props.image,
+                  price: { actual: props.actual, display: props.display },
+                })
+              : null
+          }}
+        >
+          Add to Cart
+        </Button>
+      </Grid>
     </Grid>
-    <Grid item>{props.name}</Grid>
-    <Grid item>
-      {props.actual} - {props.display}
-    </Grid>
-    <Grid item>
-      <Button
-        color="secondary"
-        onClick={() => {
-          props.cartItemList.indexOf(props.productNo) == -1
-            ? props.add({
-                productNo: props.productNo,
-                count: 1,
-                name: props.name,
-                image: props.image,
-                price: { actual: props.actual, display: props.display },
-              })
-            : null
-        }}
-      >
-        Add to Cart
-      </Button>
-    </Grid>
-  </Grid>
-)
+  )
+}
 
 Product.propTypes = {
   name: PropTypes.string,
